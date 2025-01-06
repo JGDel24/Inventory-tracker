87a0d5b2a272dce74734e7847478b1482f133163
Joel <fitness.joeld@gmail.com>
2025-01-06 08:25:36 -0500

    Inventory list added and component added to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import React, {useState} from 'react';
-
+import InventoryList from './InventoryList';
 import './App.css'
-
-
 
 function App() {
 
   const [inventory, setInventory] = useState([]);
 
   const addProduct = (product) => {
-    setInventory([...inventory, product])
+    setInventory(...inventory, product)
   }
 
-  const updateQuantity = (id, amaount) => {
+  const updateQuantity = (id, amount) => {
     setInventory(
       inventory.map((item) =>
-        item.id === id ? {...item, Quantity: item.quantity + amaount} : id
+        item.id === id ? {...item, Quantity: item.quantity + amount} : id
       )
     )
   }
+
+  return (
+    <>
+    <h1>Inventory Tracker</h1>
+    <InventoryList/>
+    
+    </>
+  )
 
 
 
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-function Product(item, updateQuantity) {
-    return (
+function Product({item, updateQuantity}){
+    return(
         <>
-        <h3>{item.name}</h3>
-        <p>Quantity: {item.quantity}</p>
-        <button onClick={() => updateQuantity(item.id, 1)}>Increase</button>
-        <button onClick={() => updateQuantity(item.id, -1)} disabled={item.quantity <= 0}>Decrease</button>
-        {item.quantity === 0 && <p>Out of Stock</p>}
+            <h3>{item.name}</h3>
+            <p>Quantity: {item.quantity}</p>
+            <button onClick={() => updateQuantity(item.id, 1)}>Increase</button>
+            <button onClick={() => updateQuantity(item.id, -1)} disabled={item.quantity <= 0}>Decrease</button>
+            {item.quantity === 0 && <p>Out of Stock</p>}
         </>
     )
 }
