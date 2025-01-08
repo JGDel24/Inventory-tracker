f5db1d7e7d0dbdc1ea7692c43bb8bc2847a59e6b
Joel <fitness.joeld@gmail.com>
2025-01-08 12:37:20 -0500

    email added to product.jsx

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 
-function Product({item, updateQuantity}) {
-    return(
+function Product(item, updateQuantity) {
+    const handleDecrease = () => {
+        if (item.quantity === 1) {
+            sendEmail(item.name)
+        }
+        updateQuantity(item.id, -1)
+    }
+
+    const sendEmail = (productName) => {
+        console.log(` Email: ${productName} is out of Stock`)
+        alert(`${productName} is out of Stock, an alert has been sent`)
+    }
+
+
+    return (
         <>
             <h3>{item.name}</h3>
-            <p>Quatity: {item.quuantity}</p>
+            <p>Quantity: {item.quantity}</p>
             <button onClick={() => updateQuantity(item.id, 1)}>Increase</button>
-            <button onClick={() => updateQuantity(item.id, -1)} disabled={item.quantity <= 0}>Decrease</button>
+            <button onClick={handleDecrease} disabled={item.quantity <= 0}>Decrease</button>
             {item.quantity === 0 && <p>Out of Stock</p>}
         </>
     )
 }
-
-export default Product
